a2eaa5b7e705a095fdc924adc02f01bfb3243611
domenico <domenico@translated.net>
2025-10-17 14:53:14 +0200

    Removed test for PHP bug

--- tests/Klein/Tests/ValidationsTest.php
+++ tests/Klein/Tests/ValidationsTest.php
@@ -381,9 +381,6 @@ class ValidationsTest extends AbstractKleinTestCase
         $this->validator('98.139.183.24')->isRemoteIp();
         $this->validator('205.186.173.52')->isRemoteIp();
 
-        // PHP BUG
-        $this->validator('2001:db8::ff00:42:8329')->isRemoteIp(); // this ip gives a false positive in PHP 8. This is an ip in the reserved range `2001:db8::/32` - IPv6 prefix for documentation purpose
-
         // Not
         $this->validator('192.168.1.1')->notRemoteIp();
         $this->validator('192.168.0.1')->notRemoteIp();
@@ -395,8 +392,6 @@ class ValidationsTest extends AbstractKleinTestCase
         $this->validator('10')->notRemoteIp();
         $this->validator('10,000')->notRemoteIp();
         $this->validator('string')->notRemoteIp();
-        $this->validator('::ffff:192.0.2.128')->notRemoteIp(); // This is an ip in the reserved range `::ffff:0:0/96` - IPv4-mapped addresses, and the IPv4 is a private reserved address
-        $this->validator('::ffff:74.125.226.192')->notRemoteIp(); // this ip gives a false positive in PHP 8. This is an ip in the reserved range `::ffff:0:0/96` - IPv4-mapped addresses and the IPv4 is a public address
     }
 
     public function testAlpha()
